fa5c0247d312a97c9384386f63f98c99a8bdd9d1
Nirali Dadhaniya <niralidadhaniya2001@gmail.com>
2022-07-14 16:58:30 +0530

    14/7/2022

---
+++ csv/csvdb.php
@@ -0,0 +1,17 @@
+<?php 
+
+$servername='localhost';
+$username='root';
+$password='';
+$dbname = "db";
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
+if(!$conn){
+   die('Could not Connect My Sql:' .mysql_error());
+}
+else
+{  
+   mysqli_select_db($conn, 'csv');  
+} 
+   
+
+?>
