a180f1cce98888b4341a72d213b6d2a4aea35232
Nirali Dadhaniya <niralidadhaniya2001@gmail.com>
2022-07-25 18:45:26 +0530

    25/7/2022

--- crud/remove.php
+++ crud/remove.php
@@ -25,21 +25,40 @@ include_once'db.php';
     //header('Location:view1.php');
 
 //} 
-if(isset($_POST['id']))
-{
-    $images = $_GET['images'];
-   // $file_name=$_FILES['files']['name'];
-    $id = $_POST['id'];
-    $sql1 = "UPDATE `crud` SET `file`='".$images."' WHERE `id`='$id'";
-    $result = $conn->query($sql1);
+// if(isset($_POST['id']))
+// {
+//     $images = $_GET['images'];
+//    // $file_name=$_FILES['files']['name'];
+//     $id = $_POST['id'];
+//     $sql1 = "UPDATE `crud` SET `file`='".$images."' WHERE `id`='$id'";
+//     $result = $conn->query($sql1);
 
-    if ($result == TRUE) 
-    {
+//     if ($result == TRUE) 
+//     {
 
-        echo "deleted";
+//         echo "deleted";
 
+//     }
+// }
+$filename = isset($_GET['id']) ? $_GET['id'] : NULL;
+
+if (!empty($filename)) {
+    $delete = unlink("photo/" . $filename);
+    if($delete){
+        $result = mysql_query("DELETE FROM crud where file="'. mysql_real_escape_string($filename)."' limit 1;")";
+        //header("Location:succes_page.php");
+            echo "success";
+    }else{
+        //header("Location:failure_page.php");
+        echo "error";
     }
 }
+else
+{
+     //header("Location:failure_page.php");
+    echo "......error....";
+}
+
 
 ?> 
 
