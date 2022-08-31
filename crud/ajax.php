f2d155b1e0e85e8fa5a7f0cc3794954b16808d18
Nirali Dadhaniya <niralidadhaniya2001@gmail.com>
2022-08-31 18:24:30 +0530

    31/8/2022

--- crud/ajax.php
+++ crud/ajax.php
@@ -8,6 +8,7 @@
     $conn=mysqli_connect('localhost','root','','db');
     if(!empty($_POST['age']))
     {
+        $_SESSION["user"]["log_id"];
         $query="SELECT * FROM crud where age=".$_POST['age']." ";
         $result=$conn->query($query);
 
@@ -18,3 +19,30 @@
                     
     }
 ?>
+<?php if(!empty($arr_users)) { ?>
+                    <?php foreach($arr_users as $user) { ?>
+                        <tr>
+                            
+                            <td><?php echo $user['id']; ?></td>
+                            <td><?php echo $user['log_id']; ?></td>
+                            <td><?php echo $user['name']; ?></td>
+                            <td><?php echo $user['age']; ?></td>
+                            <td><?php echo $user['gender']; ?></td>
+                            <td><?php echo $user['hobbies']; ?></td>
+                            <td><?php echo $user['city']; ?></td>
+                             <td>
+                                   <?php  $images=explode(',',$user["file"]); 
+                                      foreach($images as $image) {
+                                   ?>
+                                  <img src="<?php echo 'photo/'.$image; ?>" width="100" />
+                                  <?php } ?>
+                             </td>
+                             <td>
+                                <a href="update.php?id=<?php echo $user["id"]; ?>">Update</a>&nbsp;&nbsp;
+                                <a href="delete.php?id=<?php echo $user["id"]; ?>" onclick="return confirm('Are you sure?')">Delete</a>
+                            </td>
+                        </tr>
+
+                    <?php } ?>
+                    
+                <?php } ?>  
