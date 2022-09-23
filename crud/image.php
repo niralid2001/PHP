1ced1558f2701b37c51545b0bde2493f13753c87
Nirali Dadhaniya <niralidadhaniya2001@gmail.com>
2022-09-23 18:47:41 +0530

    23/9/2022

--- crud/image.php
+++ crud/image.php
@@ -10,14 +10,25 @@ $conn=mysqli_connect('localhost','root','','db');
 	$query = "select * from crud where id =".$_REQUEST["id"];
 	$result= mysqli_query($conn,$query);
 	//print_r($query);
-	while($r = mysqli_fetch_row($result))
+	while($r = mysqli_fetch_array($result))
 	{
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js">
+  </script>
+  <style type="text/css">
+  	.imgg{
+  		background-attachment:fixed;
+  	}
+  </style>
 <body>
 <center >
-<div class="col-lg-6">
-	<!-- <img class="img-fluid" src="<?php echo $r['file_name']; ?>" alt="">					 -->
-	<img src="photo/<?php echo $r['file'];?>" width="100">
+	<div class="container">
+<div class="row">
+	<div id="imgg">
+	<img src="photo/<?php echo $r['file'];?>">
+	</div>
+</div>
 </div>
 </center> 
 </body>
